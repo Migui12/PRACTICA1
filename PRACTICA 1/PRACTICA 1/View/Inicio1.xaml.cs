65fdf502858b16dfce71a45b1c70c18c2f1ced26
Migui12 <134223160+Migui12@users.noreply.github.com>
2024-08-10 13:37:35 -0500

    PRACTICA 1
    XD SASASAS

---
+++ PRACTICA 1/PRACTICA 1/View/Inicio1.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PRACTICA_1.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Inicio1 : ContentPage
+    {
+        public Inicio1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Guardar_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page2());
+        }
+    }
+}
